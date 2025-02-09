10da919759076e8c7e73db9f9e9ba15801d8556a
Gamenight77 <game.night.77176@gmail.com>
2025-02-09 11:25:13 +0100

    footer.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,4 +6,11 @@ window.onload = function() {
             document.getElementById('navbar-container').innerHTML = data;
         })
         .catch(err => console.error('Erreur de chargement de la navbar:', err));
+
+    fetch('/src/assets/html/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(err => console.error('Erreur de chargement du footer:', err));
 };
