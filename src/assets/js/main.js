5d6f31eed2e4455def76158d9588db1ae7481e00
Gamenight77 <game.night.77176@gmail.com>
2025-02-09 15:35:17 +0100

    Grid fini

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,16 @@
 // navbar.js
 let nav = null;
 window.onload = function() {
-    fetch('nav.html')
+    fetch('/src/assets/html/nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
             nav = document.getElementById("navbar");
+
         })
         .catch(err => console.error('Erreur de chargement de la navbar:', err));
-    manageNav();
+
+
 
     fetch('/src/assets/html/footer.html')
         .then(response => response.text())
